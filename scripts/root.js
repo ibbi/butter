103699b88c1be08ab867277dc9a1894c9af4344d
Ibrahim Hashme <hi@ibbi.io>
2023-01-29 15:42:43 -0500

    sidebar inject and close with relevant text

--- scripts/root.js
+++ scripts/root.js
@@ -1,19 +1,19 @@
 const getSelectedText = () => window.getSelection().toString();
 
-document.addEventListener("mouseup", (e) => {
-  if (e.ctrlKey && getSelectedText().length > 0) {
-    console.log(getSelectedText());
-  }
-});
-
 const createSidebar = () => {
   const rootElement = document.createElement("div");
-  // remove potential global styles
   rootElement.style.setProperty("all", "initial");
 
   const className = "sidebar";
   const width = 400;
   const rootShadow = rootElement.attachShadow({ mode: "closed" });
+  const closePanel = () => {
+    sidebar.style.setProperty("right", `${-width}px`);
+  };
+  const openPanel = () => {
+    sidebar.style.setProperty("right", "0px");
+  };
+
   rootShadow.innerHTML = `
         <style>
           .${className} {
@@ -28,39 +28,49 @@ const createSidebar = () => {
           }
         </style>
         <div class="${className}">
-          Hello there!
+           <button id="close" onclick="${() => closePanel()}">Close</button>
+           <p id="innerpar">
+              ${getSelectedText()}
+           </p>
         </div>
       `;
   const sidebar = rootShadow.querySelector(`.${className}`);
-  sidebar.style.setProperty("right", `${-width}px`);
+  closePanel();
 
-  let isMouseOverSidebar = false;
-  {
-    sidebar.addEventListener("mouseenter", () => {
-      isMouseOverSidebar = true;
-    });
-    sidebar.addEventListener("mouseleave", () => {
-      isMouseOverSidebar = false;
-    });
-  }
-
-  {
-    const activationAreaWidth = 100;
-    window.addEventListener("mousemove", (event) => {
-      if (event.pageX > window.innerWidth - activationAreaWidth) {
-        sidebar.style.setProperty("right", "0px");
-      } else if (!isMouseOverSidebar) {
-        sidebar.style.setProperty("right", `${-width}px`);
-      }
-    });
-  }
-
-  // close sidebar when cursor leaves the window
-  document.body.addEventListener("mouseout", (event) => {
-    if (!event.relatedTarget) {
-      sidebar.style.setProperty("right", `${-width}px`);
+  document.addEventListener("mouseup", (e) => {
+    if (e.ctrlKey && getSelectedText().length > 0) {
+      rootShadow.querySelector("#innerpar").innerHTML = getSelectedText();
+      openPanel();
     }
   });
+
+  //   let isMouseOverSidebar = false;
+  //   {
+  //     sidebar.addEventListener("mouseenter", () => {
+  //       isMouseOverSidebar = true;
+  //     });
+  //     sidebar.addEventListener("mouseleave", () => {
+  //       isMouseOverSidebar = false;
+  //     });
+  //   }
+
+  //   {
+  //     const activationAreaWidth = 100;
+  //     window.addEventListener("mousemove", (event) => {
+  //       if (event.pageX > window.innerWidth - activationAreaWidth) {
+  //         sidebar.style.setProperty("right", "0px");
+  //       } else if (!isMouseOverSidebar) {
+  //         sidebar.style.setProperty("right", `${-width}px`);
+  //       }
+  //     });
+  //   }
+
+  // close sidebar when cursor leaves the window
+  //   document.body.addEventListener("mouseout", (event) => {
+  //     if (!event.relatedTarget) {
+  //       sidebar.style.setProperty("right", `${-width}px`);
+  //     }
+  //   });
 
   return rootElement;
 };
